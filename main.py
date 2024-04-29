dd53fa3494707c2c9aabf99b39b2c9073130fd13
JoseDamiao07 <166072503+JoseDamiao07@users.noreply.github.com>
2024-04-29 00:10:12 +0000

    added some base and a choice function

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-print("What type of order is this?\n")
-print("\t1. Pick up (enter 1)")
-print("\t2. Delivery (enter 2)")
+def option_choice(min, max):
+  while True:
+    try:
+      choice = int(input("\n>>> "))
+      if min <= choice <= max:
+        return choice
+      print("\nSorry, that isn't an option. Please try again.")
+    except ValueError:
+      print("\nSorry, that isn't an option. Please try again.")
+
+print("Welcome to the Crusty Pizzas' Pizza Program!")
 while True:
-  try:
-    order_type = int(input("\n>>> "))
-    if order_type != 1 and order_type != 2:
-      print("Sorry, please type either 1 or 2.")
-    else:
-      break
-  except ValueError:
-    print("Sorry, please type either 1 or 2.")
+  print("What would you like to do?\n")
+  print("\t1. Begin an order (enter 1)")
+  print("\t2. Management summary (enter 2)")
+  print("\t3. View orders (enter 3)")
+  print("\t4. End session (enter 4)")
+  function_choice = option_choice(1, 4)
+  if function_choice == 1:
+    print("fungus1")
+  elif function_choice == 2:
+    print("fungus2")
+  elif function_choice == 3:
+    print("fungus3")
+  elif function_choice == 4:
+    break
